d33f35b8be4d483b0f3574b8474cb548919d8c84
sugar-toast <code.sugartoast@gmail.com>
2022-04-20 17:36:50 +0900

    사전스터디 20조 공부용 commit

---
+++ app.py
@@ -0,0 +1,190 @@
+import hashlib
+import datetime
+from datetime import datetime, timedelta
+import jwt  #패키지 PyJWT
+from flask import Flask, render_template,jsonify, request,redirect,url_for
+from pymongo import MongoClient
+from home import home,main,save_movies
+import certifi
+
+from detail import detail
+
+
+#암호화 키 / JWT 토큰을 사용할때 쓰는 비밀문자열
+SECRET_KEY = 'sparta20'
+
+client = MongoClient('mongodb+srv://test:sparta@cluster0.2rz7w.mongodb.net/Cluster0?retryWrites=true&w=majority', tlsCAFile=certifi.where())
+# client = MongoClient('localhost', 27017)
+db = client.netflix_comment
+app = Flask(__name__)
+
+app.register_blueprint(home)
+app.register_blueprint(detail)
+
+#jwt 체크 함수 모듈화 테스트중
+def token_check():
+    print('token_check')
+    # token_receive = request.cookies.get('mytoken')
+    # try:
+    #     payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+    #     user_info = db.user.find_one({"id": payload['id']})
+    #     return user_info['id']
+    # except jwt.ExpiredSignatureError:
+    #     print('로그인 시간만료')
+    #     return redirect(url_for('main_page'))
+    # except jwt.exceptions.DecodeError:
+    #     print('로그인 정보 없음')
+    #     return redirect(url_for('main_page'))
+
+
+#메인페이지 로그인정보가있다면 홈으로 이동 아니면 로그인이동
+@app.route('/')
+def main_page():
+    token_receive = request.cookies.get('mytoken')
+    try:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+        user_info = db.User.find_one({"id": payload['id']})
+
+        # return render_template('detail.html')
+        return redirect(url_for('home.main'))
+
+    except jwt.ExpiredSignatureError:
+        return redirect(url_for("login_page", msg="로그인 시간이 만료되었습니다."))
+
+    except jwt.exceptions.DecodeError:
+        return redirect(url_for("login_page", msg="로그인 정보가 존재하지 않습니다."))
+
+
+#jwt id 값 모듈화
+def GetJwtId():
+    # token_check()
+    token_receive = request.cookies.get('mytoken')
+    payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+    user_info = db.User.find_one({"id": payload['id']})
+    return user_info['id']
+
+# def token_check():
+#     token_receive = request.cookies.get('mytoken')
+#     try:
+#         payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+#         user_info = db.User.find_one({"id": payload['id']})
+#         return render_template('detail.html')
+#
+#     except jwt.ExpiredSignatureError:
+#         return redirect(url_for("login_page", msg="로그인 시간이 만료되었습니다."))
+#
+#     except jwt.exceptions.DecodeError:
+#         return redirect(url_for("login_page", msg="로그인 정보가 존재하지 않습니다."))
+
+
+@app.route('/')
+def hello_world():  # put application's code here
+    return redirect(url_for('login_page'))
+
+# 로그인라우터
+@app.route('/login', methods=['GET'])
+def login_page():
+    print('로그인 페이지 접속')
+
+    msg = request.args.get("msg")
+    token_receive = request.cookies.get('mytoken')
+
+    try:
+        payload = jwt.decode(token_receive,SECRET_KEY,algorithms=['HS256'])
+        user_info = db.User.find_one({"id":payload['id']})
+        return redirect(url_for('home.main'))
+        # return redirect(url_for('home_router.home'))
+
+        # return redirect(url_for('home.main'))
+    except jwt.ExpiredSignatureError:
+        return render_template('login.html',msg=msg)
+    except jwt.exceptions.DecodeError:
+        return render_template('login.html',msg=msg)
+    # except:
+    #     return render_template('login.html',msg=msg)
+
+# 회원가입 api
+@app.route('/api/register',methods=['POST'] )
+def register():
+    id_receive = request.form['id_give']
+    pw_receive = request.form['pw_give']
+
+    # pwc_receive = request.form['pwc_give']
+    #필요없나?
+
+    #sha256방법으로 암호화해서 저장
+    pw_hash = hashlib.sha256(pw_receive.encode('utf-8')).hexdigest()
+
+    doc = {
+        'id':id_receive,
+        'pw':pw_hash,
+        'fav' : []
+    }
+    db.User.insert_one(doc)
+
+    return jsonify({'msg':id_receive})
+
+#즐겨찾기 추가
+@app.route('/api/addfavorite', methods = ['POST'])
+def addfavorite():
+    fav = request.form['favorite_give']
+    id = GetJwtId()
+    dbid = db.User.find_one({'id':id})
+    db.User.update_one({'id':dbid['id']},{'$push':{'fav':{'$each':[fav],'$position':0}}})
+    # {'$push':  {'a': 5},'$position': 0}
+
+
+    return jsonify({'msg':fav})
+
+#즐겨찾기 삭제
+@app.route('/api/delfavorite', methods = ['POST'])
+def delfavorite():
+    fav = request.form['favorite_give']
+    id = GetJwtId()
+    dbid = db.User.find_one({'id':id})
+    db.User.update_one({'id':dbid['id']},{'$pull':{'fav':fav}})
+
+
+    return jsonify({'msg':fav})
+
+
+
+
+#회원가입 ID중복 확인
+@app.route('/id_dup_check',methods=['POST'])
+def id_dup_check():
+    id = request.form['id_give']
+    dup_check = bool(db.User.find_one({'id':id}))
+    return jsonify({'msg':dup_check})
+
+#로그인 성공 시 무비 데이터 로드하러 이동
+@app.route('/api/login', methods=['POST'])
+def login():
+    id_receive = request.form['id_give']
+    pw_receive = request.form['pw_give']
+
+    pw_hash = hashlib.sha256(pw_receive.encode('utf-8')).hexdigest()
+
+    result = db.User.find_one({'id':id_receive,'pw':pw_hash})
+
+    if result is not None:
+        payload = {
+            'id' : id_receive,
+            'exp' : datetime.utcnow() + timedelta(seconds = 60 * 60 * 24) #24시간 유지
+            # 'exp' : datetime.utcnow() + timedelta(seconds = 30) #test
+            # 'exp' : datetime.utcnow() + timedelta(seconds = 5) #test
+        }
+        token = jwt.encode(payload,SECRET_KEY, algorithm='HS256').decode('utf-8')
+
+
+        from home import save_movies
+        # return redirect(url_for('home.save_movies'),{'result':'success','token':token})
+
+        return jsonify({'result':'success','token':token})
+    #로그인 실패 시(아이디/비번 다름)
+    else:
+        return jsonify({'result':'fail','msg':'아이디/비밀번호가 일치하지 않습니다.'})
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
---
+++ detail.py
@@ -0,0 +1,218 @@
+import time
+import urllib
+
+from flask import render_template, Blueprint, request, jsonify,redirect,url_for
+import requests
+from bs4 import BeautifulSoup
+from urllib import parse
+import datetime
+import certifi
+
+
+
+
+
+
+from pymongo import MongoClient
+import jwt  #패키지 PyJWT
+SECRET_KEY = 'sparta20'
+
+client = MongoClient('mongodb+srv://test:sparta@cluster0.2rz7w.mongodb.net/Cluster0?retryWrites=true&w=majority', tlsCAFile=certifi.where())
+# client = MongoClient('localhost', 27017)
+db = client.netflix_comment
+
+detail = Blueprint('detail', __name__)
+
+
+@detail.route('/detail/<category>/<movie_name>')
+def main(movie_name, category):
+    from app import GetJwtId
+    TokenUserId = GetJwtId()
+
+    movie_name = urllib.parse.quote(movie_name, safe='')
+    category = parse.quote(category)
+    url = f'https://www.justwatch.com/kr/{category}/{movie_name}'
+
+    data = requests.get(url)
+    soup = BeautifulSoup(data.text, 'html.parser')
+
+    token_receive = request.cookies.get('mytoken')
+    try:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+        user_info = db.user.find_one({"id": payload['id']})
+
+        global movieTitle
+        movieTitle = soup.select_one(
+            '#base > div.jw-info-box > div > div.jw-info-box__container-content > div:nth-child(2) > div.title-block__container > div.title-block > div').text
+        print("movieTitle = " + movieTitle)
+
+        if (movieTitle) != 'None':
+            movieTitle = movieTitle
+        else:
+            movieTitle = ""
+
+        movieGenre = soup.select_one(
+            '#base > div.jw-info-box > div > div.jw-info-box__container-sidebar > div > aside > div.hidden-sm.visible-md.visible-lg.title-sidebar__desktop > div.title-info > div:nth-child(3) > div.detail-infos__value').text
+        print("movieGenre = " + movieGenre)
+
+        if (movieGenre) != 'None':
+            movieGenre = movieGenre
+        else:
+            movieGenre = ""
+
+        movieTime = soup.select_one(
+            '#base > div.jw-info-box > div > div.jw-info-box__container-sidebar > div > aside > div.hidden-sm.visible-md.visible-lg.title-sidebar__desktop > div.title-info > div:nth-child(4) > div.detail-infos__value').text
+        print("movieTime = " + movieTime)
+
+        if (movieTime) != 'None':
+            movieTime = movieTime
+        else:
+            movieTime = ""
+
+
+        movieMainThumbnail = soup.select_one(
+            '#base > div.backdrops > div > div > div:nth-child(2) > picture > img')
+        print("dramaMainThumbnail = " + str(movieMainThumbnail))
+
+        movieMainThumbnail2 = soup.select_one(
+            '#base > div.backdrops.backdrops__carousel > div > div.swiper-container > div > div.swiper-slide.swiper-slide-active > picture > img')
+        print("movieMainThumbnail2 = "+ str(movieMainThumbnail2))
+
+        movieMainThumbnail3 = soup.select_one(
+             '#base > div.backdrops.backdrops__carousel > div > div.swiper-container > div > div > picture > img')
+        print("movieMainThumbnail3 = "+ str(movieMainThumbnail3))
+
+        if str(movieMainThumbnail) != 'None':
+            movieMainThumbnail = movieMainThumbnail["src"]
+
+        elif str(movieMainThumbnail2) != 'None':
+            movieMainThumbnail = movieMainThumbnail2["src"]
+        elif str(movieMainThumbnail3) != 'None':
+            movieMainThumbnail = movieMainThumbnail3["src"]
+        else:
+            movieMainThumbnail = ""
+
+
+        movieImage = soup.select_one(
+            '#base > div.jw-info-box > div > div.jw-info-box__container-sidebar > div > aside > div.hidden-xs.visible-sm.hidden-md.hidden-lg.title-sidebar__desktop > div > picture > source:nth-child(1)')[
+            "data-srcset"].split(',')[0]
+
+        if(movieImage) !='None':
+            movieImage = movieImage
+        else:
+            movieImage = ""
+
+        ###base > div.jw-info-box > div > div.jw-info-box__container-sidebar > div > aside > div.hidden-sm.visible-md.visible-lg.title-sidebar__desktop > div.title-poster.title-poster--no-radius-bottom > picture > source:nth-child(1)
+        print("movieImage = " + movieImage)
+
+        movieSummary = soup.select_one(
+            '#base > div.jw-info-box > div > div.jw-info-box__container-content > div:nth-child(2) > div:nth-child(6) > div:nth-child(1) > div:nth-child(4) > p > span')
+
+        dramaSummary = soup.select_one(
+            '#base > div.jw-info-box > div > div.jw-info-box__container-content > div:nth-child(2) > div:nth-child(7) > div:nth-child(1) > div:nth-child(4) > p > span')
+
+
+
+        if str(movieSummary) != 'None':
+            movieSummary = movieSummary.text
+            print(movieSummary)
+
+        elif str(dramaSummary) != 'None':
+            movieSummary = dramaSummary.text
+            print(dramaSummary)
+        else:
+            movieSummary = ""
+
+        read_reviews()
+        
+        return render_template('detail.html', TokenUserId=TokenUserId, movieTitle=movieTitle, movieGenre=movieGenre,
+                               movieTime=movieTime,
+                               movieSummary=movieSummary, movieImage=movieImage,movieMainThumbnail=movieMainThumbnail, reviews=reviews)
+
+        
+
+
+    except jwt.ExpiredSignatureError:
+        print('로그인 시간만료')
+        return redirect(url_for('login_page'))
+    except jwt.exceptions.DecodeError:
+        print('로그인 정보 없음')
+        return redirect(url_for('login_page'))
+
+@detail.route('/review', methods=['POST'])
+def write_review():
+    from app import GetJwtId
+    global TokenUserId
+    TokenUserId = GetJwtId()
+    now = datetime.datetime.now()
+    review = request.form['Review']
+    review = review.replace("  ", " ")
+    starValue = request.form['starValue']
+    movietitle = request.form['movieTitle']
+
+    if starValue != "" and review != "":
+        nowDatetime = now.strftime('%Y-%m-%d %H:%M:%S')
+
+        doc = {
+            'userId': TokenUserId,
+            'Review': review,
+            'starValue': starValue,
+            'movieTitle': movietitle,
+            'nowDatetime': nowDatetime
+        }
+        db.review.insert_one(doc)
+        return jsonify({'msg': '리뷰 등록 완료'})
+
+
+@detail.route('/review', methods=['GET'])
+def read_reviews():
+    global reviews
+    reviews = list(db.review.find({"movieTitle": movieTitle}, {'_id': False}))
+    print(reviews)
+    return jsonify({'msg': '리뷰 조회'})
+
+
+@detail.route('/review', methods=['Delete'])
+def del_reviews():
+    from app import GetJwtId
+    TokenUserId = GetJwtId()
+    userId = request.form['userid']
+    Review = request.form['review']
+    starValue = request.form['starValue']
+    nowDatetime = request.form['writeTime']
+    if (TokenUserId == userId):
+        db.review.delete_one({'userId': userId, 'nowDatetime': nowDatetime, 'Review': Review, 'starValue': starValue})
+        review = db.review.find({'movieTitle': movieTitle})
+        print(review)
+        return jsonify({'msg': '삭제완료'})
+    else:
+        return jsonify({'msg': '본인이 작성한 리뷰만 삭제가능합니다.'})
+
+
+@detail.route('/review', methods=['Put'])
+def update_reviews():
+    from app import GetJwtId
+    TokenUserId = GetJwtId()
+    userId = request.form['userid']
+    Review = request.form['review']
+    starValue = request.form['starValue']
+    nowDatetime = request.form['writeTime']
+    updateReview = request.form['updateReview']
+    updateStarValue = request.form['updateStarValue']
+    print("userId = " + userId + "Review = " + Review + "starValue = " + starValue + "nowDatetime = " + nowDatetime)
+    if (TokenUserId == userId):
+        db.review.update_one({'userId': userId, 'nowDatetime': nowDatetime, 'Review': Review, 'starValue': starValue},
+                             {'$set': {'Review': updateReview, 'starValue': updateStarValue}})
+        review = db.review.find({'movieTitle': movieTitle})
+        print(review)
+        return jsonify({'msg': '수정완료'})
+    else:
+        return jsonify({'msg': '본인이 작성한 리뷰만 수정가능합니다.'})
+
+
+@detail.route('/allReview', methods=['GET'])
+def read_all_Reviews():
+    global reviews
+    reviews = list(db.review.find({}, {'_id': False}))
+    print(reviews)
+    return jsonify({'reviews': reviews})
